adb4b16d62bffd734018477808cea2f509810e78
yazoo1220 <podman1220@hotmail.com>
2023-05-18 00:43:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,32 +11,32 @@ from langchain.schema import (
 from langchain.memory import ConversationBufferMemory
 import openai
 
-st.header("AMA for Eurovision")
+st.header("AMA")
 st.subheader("Streamlit + ChatGPT + Langchain with `stream=True`")
 
 user_input = st.text_input("You: ",placeholder = "Ask me anything ...", key="input")
 
 
-loader = UnstructuredURLLoader(["https://en.wikipedia.org/wiki/Eurovision_Song_Contest"])
-documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
-embeddings = OpenAIEmbeddings()
-docsearch = Chroma.from_documents(docs, embeddings)
+# loader = UnstructuredURLLoader(["https://en.wikipedia.org/wiki/Eurovision_Song_Contest"])
+# documents = loader.load()
+# text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+# docs = text_splitter.split_documents(documents)
+# embeddings = OpenAIEmbeddings()
+# docsearch = Chroma.from_documents(docs, embeddings)
 handler = StreamlitCallbackHandler()
 memory = ConversationBufferMemory()
 
-def gen_prompt(docs, query) -> str:
-    return f"""To answer the question please only use the Context given, nothing else. Do not make up answer, simply say 'I don't know' if you are not sure.
-Question: {query}
-Context: {[doc.page_content for doc in docs]}
-Answer:
-"""
+# def gen_prompt(docs, query) -> str:
+#     return f"""To answer the question please only use the Context given, nothing else. Do not make up answer, simply say 'I don't know' if you are not sure.
+# Question: {query}
+# Context: {[doc.page_content for doc in docs]}
+# Answer:
+# """
 
-def prompt(query):
-     docs = docsearch.similarity_search(query, k=4)
-     prompt = gen_prompt(docs, query)
-     return prompt
+# def prompt(query):
+#      docs = docsearch.similarity_search(query, k=4)
+#      prompt = gen_prompt(docs, query)
+#      return prompt
 
 if st.button("Submit", type="primary"):
     st.markdown("----")
