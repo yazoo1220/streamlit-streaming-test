7aa1c2cd489e05303469755311aafaaf07033911
yazoo1220 <podman1220@hotmail.com>
2023-05-18 17:19:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ memory = ConversationBufferMemory(memory_key="chat_history", return_messages=Tru
 
 prompt = PromptTemplate(
     input_variables=["chat_history","question"], 
-    template='Based on the following chat_history, Please reply to the question in format of markdown. chat_history: {chat_history}, question:{question}'
+    template='Based on the following chat_history, Please reply to the question in format of markdown. chat_history: {chat_history}'
 )
 
 user_input = st.text_input("You: ",placeholder = "Ask me anything ...", key="input")
