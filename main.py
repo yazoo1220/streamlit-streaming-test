1413790a6efee695b125b6ba6db7fee942f0155a
yazoo1220 <podman1220@hotmail.com>
2023-05-18 13:45:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from langchain. chat_models import ChatOpenAI
 import streamlit as st
+from langchain. chat_models import ChatOpenAI
+from langchain import PromptTemplate
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.document_loaders import UnstructuredURLLoader
 from langchain.text_splitter import CharacterTextSplitter
