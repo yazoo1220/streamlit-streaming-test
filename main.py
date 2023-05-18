bf347fb9d9c9553f6bbe5f91e9b38b1e51984bfe
yazoo1220 <podman1220@hotmail.com>
2023-05-18 22:57:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,25 +5,13 @@ from langchain.callbacks.base import BaseCallbackHandler
 from langchain.schema import (
     HumanMessage,
 )
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.document_loaders import UnstructuredURLLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import Chroma
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 import openai
 from typing import Any, Dict, List
 
 st.header("AMA")
 st.subheader("Streamlit + ChatGPT + Langchain with `stream=True`")
-
-loader = UnstructuredURLLoader(["https://en.wikipedia.org/wiki/Eurovision_Song_Contest"])
-documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
-embeddings = OpenAIEmbeddings()
-db = Chroma.from_documents(docs, embeddings)
-retriever = db.as_retriever()
                                   
 def get_state(): 
      if "state" not in st.session_state: 
@@ -34,7 +22,7 @@ st.write(state)
 st.write(state['memory'])
 
 prompt = PromptTemplate(
-    input_variables=["chat_history","question"], 
+    input_variables=["history","question"], 
     template='Based on the following chat_history, Please reply to the question in format of markdown. history: {chat_history}. question: {question}'
 )
 
@@ -61,12 +49,12 @@ if ask:
     with st.spinner('typing...'):
         report = []
         chat = ChatOpenAI(streaming=True, temperature=0.9)
-        qa = ConversationalRetrievalChain.from_llm(
+        conversation = ConversationChain(
             llm=chat, 
             prompt=prompt,
             retriever=retriever,
             memory=state['memory']            
         )
-        res = qa({question:user_input, callbacks:[handler]})
+        res = conversation(question=user_input, callbacks=[handler])
     
 st.markdown("----")
