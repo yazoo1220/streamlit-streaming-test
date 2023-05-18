fefa1bab2d743c311e27dd67f79f8655db68b8c6
yazoo1220 <podman1220@hotmail.com>
2023-05-18 14:28:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ prompt = PromptTemplate(
     template='Based on the following chat_history, Please reply to the question in format of markdown. chat_history: {chat_history},\n question: {question}'
 )
 
-user_input = st.text_input("You: ",placeholder = "Ask me anything ...", key="input")
+user_input = st.text_input("You: ",placeholder = "Ask me anything ...", key="question")
 ask = st.button('ask',type='primary')
 st.markdown("----")
 
@@ -58,6 +58,6 @@ if ask:
             prompt=prompt,
             memory=memory            
         )
-        res = conversation.predict(input=user_input, callbacks=[handler])
+        res = conversation.predict(question=user_input, callbacks=[handler])
     
 st.markdown("----")
